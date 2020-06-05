d673072620d39d6567a69db951ea6bd2ea127860
romazpua <roma.zp.ua@gmail.com>
2020-06-05 18:55:52 +0300

    create new work lesson29 2020/06/05

---
+++ lesson29/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.scss";
+
+// function App() {
+//   return <div className="App">Functional Component</div>;
+// }
+
+class App extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
+}
+
+export default App;
