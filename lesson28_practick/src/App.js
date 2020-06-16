9b86e7f508e6395fb0bf6afe423c7c92356c9298
romazpua <roma.zp.ua@gmail.com>
2020-06-16 18:35:57 +0300

    new components

--- lesson28_practick/src/App.js
+++ lesson28_practick/src/App.js
@@ -6,10 +6,19 @@ import OurComponent from "./components/CustomComponents/OurComponents/index";
 import {v4 as uuidv4} from 'uuid';
 import Text from "./components/CustomComponents/OurComponents/text";
 import FuncEl from "./components/CustomComponents/OurComponents/FuncEl/index";
-import Nav from "./components/CustomComponents/OurComponents/Nav/index";
+import Nav from "./components/Nav/index";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Posts from "./components/CustomComponents/OurComponents/Posts/index";
 import FuncComponent from "./components/FuncComponent";
+import {
+  Switch,
+  Route, Redirect, NavLink, Link,
+} from "react-router-dom";
+import Users from "./components/Users";
+import {User} from "./components/User";
+import {NewPost} from "./components/NewPost/index";
+import NewPosts from "./components/NewPosts";
+
 
 // function App() {
 //   return <div className="App">Functional Component</div>;
@@ -21,6 +30,8 @@ class App extends React.Component {
     currentTab: 'in progress',
     users: [],
     isLoading: true,
+    userName: '',
+    age: '',
     todos: [
       {
         id: 1,
@@ -49,6 +60,17 @@ class App extends React.Component {
     ]
   }
 
+  inputH = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  submitH = event => {
+    event.preventDefault();
+    console.log(this.state.userName, this.state.age)
+  }
+
   componentDidMount() {
 
     fetch('https://jsonplaceholder.typicode.com/users/')
@@ -57,7 +79,7 @@ class App extends React.Component {
         this.setState({users: json, isLoading: false})
       })
   }
-  
+
   inputHandler = event => {
     this.setState({
       newTodoName: event.target.value
@@ -124,7 +146,7 @@ class App extends React.Component {
 
 
   render() {
-    const {newTodoName, todos, users, isLoading} = this.state
+    const {newTodoName, todos, users, isLoading, userName, age} = this.state
     const allTodos = todos
     const deletedTodos = todos.filter(el => el.deleted === true)
     const inProgressTodos = todos.filter(el => (el.done === false && el.deleted === false))
@@ -148,73 +170,135 @@ class App extends React.Component {
         todosArr = allTodos
     }
 
+
     return (
       < div className="App">
-        <FuncComponent uorNewProp={28}/>
-        <Nav rows={[1,2,3,4]} />
 
-        {
-          isLoading
-          ? <CircularProgress size={30} thickness={5}/>
-            : (
-              <>
-                {users.map(el => (
-                  <p key={el.id}>{el.name}</p>
+        <nav>
+          <NavLink to="/">Главная</NavLink>
+          <NavLink to="/users">Юзеры</NavLink>
+          <NavLink to="/posts">Посты</NavLink>
+          <NavLink to="old-lessons">Old lessons</NavLink>
+        </nav>
+
+        <Switch>
+
+          <Route path="/old-lessons">
+            <>
+              OLD-LESSONS
+              <form onSubmit={this.submitH}>
+                <input
+                  type="text"
+                  name='userName'
+                  onChange={this.inputH}
+                  value={userName}/>
+                <input
+                  type="text"
+                  name='age'
+                  onChange={this.inputH}
+                  value={age}/>
+                <button type="submit">
+                  Отправить
+                </button>
+              </form>
+
+              <FuncComponent uorNewProp={28}/>
+              <Nav rows={[1, 2, 3, 4]}/>
+
+              {
+                isLoading
+                  ? <CircularProgress size={30} thickness={5}/>
+                  : (
+                    <>
+                      {users.map(el => (
+                        <p key={el.id}>{el.name}</p>
+                      ))}
+                    </>
+                  )
+              }
+
+              <Posts/>
+
+              <h1>Todo app</h1>
+              <div className="filter">
+                <Button variant='contained' onClick={() => this.filter('all')}>All</Button>
+                <Button variant='contained' color='primary' onClick={() => this.filter('in progress')}>In
+                  progerss</Button>
+                <ColorButton variant="contained" onClick={() => this.filter('done')}>
+                  Done
+                </ColorButton>
+                <Button variant='contained' color='secondary' onClick={() => this.filter('deleted')}>Deleted</Button>
+              </div>
+              <div className="todos">
+                {todosArr !== null && todosArr.map(todo => (
+                  <div className={`todo ${todo.done ? "done" : ""}`}
+                       key={todo.id}>
+                    <Checkbox color="primary" checked={todo.done}
+                              onChange={(event) => this.doneTodo(event, todo.id)}/>
+                    <p>{todo.text}</p>
+                    <Button
+                      color='primary'
+                      variant='contained'
+                      onClick={() => this.editTodo(todo.id)}>
+                      Edit
+                    </Button>
+                    <Button
+                      color='secondary'
+                      variant='outlined'
+                      onClick={() => this.deleteTodo(todo.id)}>
+                      Delete
+                    </Button>
+                  </div>
                 ))}
-              </>
-            )
-        }
+              </div>
+              {this.state.currentTab !== 'deleted' && (
+                <div className="addTodo">
+                  <input type="text" value={newTodoName} onChange={this.inputHandler}/>
+                  <Button color='primary'
+                          variant='contained'
+                          disabled={!newTodoName}
+                          onClick={this.addTodo}>
+                    Add todo
+                  </Button>
+                </div>)}
 
-        <Posts/>
+              <OurComponent number={uuidv4()} text={'dfdf'}/>
+              <OurComponent number={uuidv4()} text={'dfbbdfsbsfdbsd'}/>
+              <Text text='Hello'/>
+              <Text text='World'/>
+              <FuncEl number={1} text={'bla-bla'}/>
+              <FuncEl number={2} text={'bla-bla-bla'}/>
+            </>
+          </Route>
 
-        <h1>Todo app</h1>
-        <div className="filter">
-          <Button variant='contained' onClick={() => this.filter('all')}>All</Button>
-          <Button variant='contained' color='primary' onClick={() => this.filter('in progress')}>In progerss</Button>
-          <ColorButton variant="contained" onClick={() => this.filter('done')}>
-            Done
-          </ColorButton>
-          <Button variant='contained' color='secondary' onClick={() => this.filter('deleted')}>Deleted</Button>
-        </div>
-        <div className="todos">
-          {todosArr !== null && todosArr.map(todo => (
-            <div className={`todo ${todo.done ? "done" : ""}`}
-                 key={todo.id}>
-              <Checkbox color="primary" checked={todo.done}
-                        onChange={(event) => this.doneTodo(event, todo.id)}/>
-              <p>{todo.text}</p>
-              <Button
-                color='primary'
-                variant='contained'
-                onClick={() => this.editTodo(todo.id)}>
-                Edit
-              </Button>
-              <Button
-                color='secondary'
-                variant='outlined'
-                onClick={() => this.deleteTodo(todo.id)}>
-                Delete
-              </Button>
-            </div>
-          ))}
-        </div>
-        {this.state.currentTab !== 'deleted' && (
-          <div className="addTodo">
-            <input type="text" value={newTodoName} onChange={this.inputHandler}/>
-            <Button color='primary'
-                    variant='contained'
-                    disabled={!newTodoName}
-                    onClick={this.addTodo}>
-              Add todo
-            </Button>
-          </div>)}
+          <Route exact path="/users">
+            <Users/>
+          </Route>
 
-        <OurComponent number={uuidv4()} text={'dfdf'}/>
-        <OurComponent number={uuidv4()} text={'dfbbdfsbsfdbsd'}/>
-        <Text text='Hello'/>
-        <Text text='World'/>
-        <FuncEl number={1} text={'bla-bla'}/>
-        <FuncEl number={2} text={'bla-bla-bla'}/>
+          <Route exact path="/posts">
+            <NewPosts/>
+          </Route>
+
+          <Route exact path="/posts/:id">
+            <NewPost/>
+          </Route>
+
+          <Route path='/users/:id'>
+            <User/>
+          </Route>
+
+          <Route exact path="/">
+            H O M E
+          </Route>
+
+          {/*<Route path='*'>*/}
+          {/*  404*/}
+          {/*</Route>*/}
+
+          {/*<Redirect from='*' to='/'/>*/}
+
+        </Switch>
+
       </div>
     );
   }
--- lesson31/src/App.js
+++ lesson31/src/App.js
@@ -1,22 +1,76 @@
 import React from "react";
 import "./App.scss";
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+import Login from "./components/login";
 
 // function App() {
 //   return <div className="App">Functional Component</div>;
 // }
 
 class App extends React.Component {
-    state = {
+  state = {
+    userName: '',
+    age: '',
+  }
 
-    }
+  inputH = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
-    render() {
-        return (
-            <div className="App">
+  submitH = event => {
+    event.preventDefault()
+    console.log(this.state.userName, this.state.age)
+  }
 
-            </div>
-        );
-    }
+
+  render() {
+    const {userName, age} = this.state
+    return (
+      <div className="App">
+
+        <Login/>
+
+        <Switch>
+
+
+          <Route path="/old-lessons">
+            <form onSubmit={this.submitH}>
+              <input
+                type="text"
+                name='userName'
+                value={userName}
+                onChange={this.inputH}/>
+              <input
+                type="text"
+                name='age'
+                value={age}
+                onChange={this.inputH}/>
+              <button type='submit'>
+                Отправить
+              </button>
+            </form>
+          </Route>
+
+          <Route path='/users'>
+            users
+          </Route>
+
+          <Route path="/">
+            home
+          </Route>
+
+
+        </Switch>
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
